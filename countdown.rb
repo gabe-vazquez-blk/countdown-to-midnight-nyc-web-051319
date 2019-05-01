0362d7b3b03979c239ac75d73acc1b27231d80b4
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-01 13:05:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(number)
   while number > 0 
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
